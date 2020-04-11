2c771b88c6c4c73eca26b20e6ac9f412c2001c7a
Emanuel Rodrigues <raulafonso88@gmail.com>
2020-04-11 15:45:07 +0100

    footer refactored for reusabilityy

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php include "includes/site_layout/header.php"; ?>
+    <?php include "includes/site_layout/header.php"; ?>
 
     <!-- Navigation -->
     <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
@@ -170,25 +170,4 @@
         <hr>
 
         <!-- Footer -->
-        <footer>
-            <div class="row">
-                <div class="col-lg-12">
-                    <p>Copyright &copy; Your Website 2014</p>
-                </div>
-                <!-- /.col-lg-12 -->
-            </div>
-            <!-- /.row -->
-        </footer>
-
-    </div>
-    <!-- /.container -->
-
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
-
-</body>
-
-</html>
+    <?php include "includes/site_layout/footer.php"; ?>
