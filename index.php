2b2368c13a95744e10e95e012299c169c684953f
Emanuel Rodrigues <raulafonso88@gmail.com>
2020-04-11 16:04:40 +0100

    components created

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
     <!-- Header component -->
-    <?php include "includes/site_layout/header.php"; ?>
+    <?php include "includes/components/header.php"; ?>
 
     <!-- Navigation component-->
-    <?php include "includes/site_layout/navigation.php"; ?>
+    <?php include "includes/components/navigation.php"; ?>
 
 
     <!-- Page Content -->
@@ -47,64 +47,8 @@
 
             </div>
 
-            <!-- Blog Sidebar Widgets Column -->
-            <div class="col-md-4">
-
-                <!-- Blog Search Well -->
-                <div class="well">
-                    <h4>Blog Search</h4>
-                    <div class="input-group">
-                        <input type="text" class="form-control">
-                        <span class="input-group-btn">
-                            <button class="btn btn-default" type="button">
-                                <span class="glyphicon glyphicon-search"></span>
-                        </button>
-                        </span>
-                    </div>
-                    <!-- /.input-group -->
-                </div>
-
-                <!-- Blog Categories Well -->
-                <div class="well">
-                    <h4>Blog Categories</h4>
-                    <div class="row">
-                        <div class="col-lg-6">
-                            <ul class="list-unstyled">
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <!-- /.col-lg-6 -->
-                        <div class="col-lg-6">
-                            <ul class="list-unstyled">
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <!-- /.col-lg-6 -->
-                    </div>
-                    <!-- /.row -->
-                </div>
-
-                <!-- Side Widget Well -->
-                <div class="well">
-                    <h4>Side Widget Well</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
-                </div>
-
-            </div>
+    <!-- Blog Sidebar Widgets Column component-->
+    <?php include "includes/components/sidebar.php"; ?>
 
         </div>
         <!-- /.row -->
@@ -112,4 +56,4 @@
         <hr>
 
         <!-- Footer component-->
-    <?php include "includes/site_layout/footer.php"; ?>
+    <?php include "includes/components/footer.php"; ?>
