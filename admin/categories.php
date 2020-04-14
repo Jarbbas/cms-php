d47ea7414d50e051e6327501bbfb17f1c6f23b2f
Emanuel <raulafonso88@gmail.com>
2020-04-14 09:46:44 +0100

    created a categories section
    Categories section with a simple form

--- admin/categories.php
+++ admin/categories.php
@@ -1,13 +1,37 @@
-<?php include "includes/components/header.php";
+<?php
+//root pathsand DB
+$path_to_db = $_SERVER['DOCUMENT_ROOT'];
+$path_to_db .= "/projeto-cms-php/includes/db.php";
+$path_to_functions = $_SERVER['DOCUMENT_ROOT'];
+$path_to_functions .= "/projeto-cms-php/includes/functions.php";
+$path_to_messages = $_SERVER['DOCUMENT_ROOT'];
+$path_to_messages .= "/projeto-cms-php/includes/messages.php";
 
-if($connection) echo "SUCESS!";
+
+$path_to_header_admin = $_SERVER['DOCUMENT_ROOT'];
+$path_to_header_admin .= "/projeto-cms-php/admin/includes/components/header.php";
+$path_to_navigation_admin = $_SERVER['DOCUMENT_ROOT'];
+$path_to_navigation_admin .= "/projeto-cms-php/admin/includes/components/navigation.php";
+$path_to_footer_admin = $_SERVER['DOCUMENT_ROOT'];
+$path_to_footer_admin .= "/projeto-cms-php/admin/includes/components/footer.php";
+
+include_once($path_to_db);
+include_once($path_to_functions);
+include_once($path_to_messages);
+
+//header component
+include_once($path_to_header_admin);
+
+
 
 ?>
     <div id="wrapper">
 
         <!-- Navigation -->
 
-        <?php include "includes/components/navigation.php"; ?>
+<?php 
+    include_once($path_to_navigation_admin); 
+?>
 
         <div id="page-wrapper"> 
 
@@ -17,17 +41,33 @@ if($connection) echo "SUCESS!";
                 <div class="row">
                     <div class="col-lg-12">
                         <h1 class="page-header">
-                            Admin Page
-                            <small>conteudo administrativo</small>
+                            Welcome Admin
+                            <small>Author</small>
                         </h1>
-                        <ol class="breadcrumb">
+
+                        <div class="col-xs-6">
+                        <form action="">
+                            <div class="form-group">
+                            <label for="cat-title">Add Category</label>
+                                <input type="text" class="form-control" name="cat-title">
+                            </div>
+                            <div class="form-group">
+                                <input type="submit" class="btn btn-primary" name="submit" value="Add Categorie">
+                            </div>
+
+
+                        </form>                     
+                        </div>
+
+
+                        <!-- <ol class="breadcrumb">
                             <li>
                                 <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
                             </li>
                             <li class="active">
                                 <i class="fa fa-file"></i> Blank Page
                             </li>
-                        </ol>
+                        </ol> -->
                     </div>
                 </div>
                 <!-- /.row -->
@@ -41,4 +81,6 @@ if($connection) echo "SUCESS!";
     </div>
     <!-- /#wrapper -->
     
-<?php include "includes/components/footer.php"; ?>
+<?php 
+    include_once($path_to_footer_admin); 
+?>
