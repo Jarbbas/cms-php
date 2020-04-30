29df42c875549a41c6ec7089979f81a44e9abb6d
Emanuel <raulafonso88@gmail.com>
2020-04-30 17:06:43 +0100

    users main page created

---
+++ admin/users.php
@@ -0,0 +1,65 @@
+    <?php
+     //header component
+    $path_to_header_admin = $_SERVER['DOCUMENT_ROOT'];
+    $path_to_header_admin .= "/cms-php/admin/includes/components/header.php";
+    include_once($path_to_header_admin);
+    ?>
+
+    <div id="wrapper">
+
+    <!-- Navigation -->
+
+    <?php  include_once($path_to_navigation_admin); ?>
+
+    <div id="page-wrapper">
+    <div class="container-fluid">
+
+    <!-- Page Heading -->
+    <div class="row">
+
+    <div class="col-lg-12">
+
+    <h1 class="page-header">
+      Comments Page
+    <small>Author</small>
+    </h1>
+
+
+
+<?php
+
+if (isset($_GET['source'])) {
+
+    $source = $_GET['source'];
+} else {
+    $source = "";
+}
+
+    switch ($source) {
+
+        case 'add':
+            include "includes/components/add_user.php";
+            break;
+
+        case 'edit':
+            include "includes/components/edit_user.php";
+            break;
+
+        case 'view':
+            include "includes/components/view_all_users.php";
+            break;
+
+        default:
+            include "includes/components/view_all_users.php";
+            break;
+    }
+
+?>
+
+    </div>
+    <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+    <?php include_once($path_to_footer_admin); ?>
