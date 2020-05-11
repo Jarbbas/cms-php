5f3f2a12c5e8ac729ca9fcf15fe8a09408641d1a
Emanuel <raulafonso88@gmail.com>
2020-05-11 10:20:22 +0100

    mysql DB updated

--- cms.sql
+++ cms.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: 29-Abr-2020 às 15:41
--- Versão do servidor: 10.1.36-MariaDB
--- versão do PHP: 7.2.11
+-- Host: localhost
+-- Generation Time: May 11, 2020 at 11:19 AM
+-- Server version: 8.0.20-0ubuntu0.20.04.1
+-- PHP Version: 7.4.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -25,53 +24,102 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `categories`
+-- Table structure for table `categories`
 --
 
 CREATE TABLE `categories` (
-  `cat_id` int(3) NOT NULL,
+  `cat_id` int NOT NULL,
   `cat_title` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Extraindo dados da tabela `categories`
+-- Dumping data for table `categories`
 --
 
 INSERT INTO `categories` (`cat_id`, `cat_title`) VALUES
 (1, 'PHP'),
-(2, 'HTML'),
-(3, 'Javascript'),
-(15, 'CSS3');
+(2, 'Javascript'),
+(3, 'HTML'),
+(6, 'React'),
+(7, 'Biography');
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `posts`
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `comment_id` int NOT NULL,
+  `comment_post_id` int NOT NULL,
+  `comment_author` varchar(255) NOT NULL,
+  `comment_email` varchar(255) NOT NULL,
+  `comment_content` text NOT NULL,
+  `comment_status` varchar(255) NOT NULL,
+  `comment_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `comments`
+--
+
+INSERT INTO `comments` (`comment_id`, `comment_post_id`, `comment_author`, `comment_email`, `comment_content`, `comment_status`, `comment_date`) VALUES
+(3, 1, 'Admin', 'admin@mail.com', 'teste', 'unapproved', '2020-04-27');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
 --
 
 CREATE TABLE `posts` (
-  `post_id` int(3) NOT NULL,
-  `post_category_id` int(3) NOT NULL,
+  `post_id` int NOT NULL,
+  `post_category_id` int NOT NULL,
   `post_title` varchar(255) NOT NULL,
   `post_author` varchar(255) NOT NULL,
   `post_date` date NOT NULL,
   `post_image` text NOT NULL,
   `post_content` text NOT NULL,
   `post_tags` varchar(255) NOT NULL,
-  `post_comment_count` int(11) NOT NULL,
-  `post_status` varchar(255) NOT NULL DEFAULT 'draft'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `post_comment_count` int DEFAULT NULL,
+  `post_status` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Extraindo dados da tabela `posts`
+-- Dumping data for table `posts`
 --
 
 INSERT INTO `posts` (`post_id`, `post_category_id`, `post_title`, `post_author`, `post_date`, `post_image`, `post_content`, `post_tags`, `post_comment_count`, `post_status`) VALUES
-(1, 1, 'Emanuel\'s CMS PHP Project', 'Emanuel Rodrigues', '2020-04-12', 'waste_no_more_time_arguing_be_one.png', 'This is content for this post, wich will beeeee awosommmmme!!', 'emanuel, cms, php', 0, 'draft'),
-(2, 2, 'PHP course', 'Edwins', '2020-04-13', '84248.png', 'this is a CMS PHP course hosted by edwins in his udemy page', 'udemy, php, edwins', 0, 'draft'),
-(3, 3, 'Eddisrupt Bootcamp', 'Rui Nogueira', '2018-09-06', 'your_custom_beautiful_quotes_here.png\r\n', 'This was a Javascript, html, CSS and React in Lisbon, and was my first experience', 'javascript, react, html, css, bootcamp', 0, 'draft'),
-(5, 1, 'final update', 'Admin', '2020-04-24', '167-1677350_sticker-cutout-head-sculpture-greek-greece-vaporwave-marcus.png', 'teste 4', 'Javascript ', 1, 'Updated'),
-(6, 3, 'New Post', 'Emanuel', '2020-04-24', 'angre_angular_TCL.jpg', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', 'Javascript, book, code, learning', 1, 'Pending');
+(1, 1, 'This is a Post made in my Ubunto 20', 'Emanuel', '2020-04-25', '84248.png', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', 'PHP, Linux', 1, 'published'),
+(2, 1, 'Este é um post sobre Linux', 'Emanuel', '2020-04-26', '20200122_091237-01.jpeg', 'under construction', 'Linux', 1, 'published'),
+(3, 7, 'Marcus Aurelius', 'Marcus', '2020-05-10', 'bi_graphics_rules-for-being-an-exceptional-leader-from-marcus-aurelius_top_03.png', 'Meditations (Medieval Greek: Τὰ εἰς ἑαυτόν, romanized: Ta eis heauton, literally \"things to one\'s self\") is a series of personal writings by Marcus Aurelius, Roman Emperor from 161 to 180 AD, recording his private notes to himself and ideas on Stoic philosophy.\r\n\r\nMarcus Aurelius wrote the 12 books of the Meditations in Koine Greek[1] as a source for his own guidance and self-improvement.[2] It is possible that large portions of the work were written at Sirmium, where he spent much time planning military campaigns from 170 to 180. Some of it was written while he was positioned at Aquincum on campaign in Pannonia, because internal notes tell us that the first book was written when he was campaigning against the Quadi on the river Granova (modern-day Hron) and the second book was written at Carnuntum.\r\n\r\nIt is unlikely that Marcus Aurelius ever intended the writings to be published and the work has no official title, so \"Meditations\" is one of several titles commonly assigned to the collection. These writings take the form of quotations varying in length from one sentence to long paragraphs. ', 'Meditation, bio', NULL, 'draft');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `user_password` varchar(255) NOT NULL,
+  `user_fristname` varchar(255) NOT NULL,
+  `user_lastname` varchar(255) NOT NULL,
+  `user_email` varchar(255) NOT NULL,
+  `user_image` text NOT NULL,
+  `user_role` varchar(255) NOT NULL,
+  `randSalt` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `username`, `user_password`, `user_fristname`, `user_lastname`, `user_email`, `user_image`, `user_role`, `randSalt`) VALUES
+(1, 'emanuel', 'root', 'Emanuel', 'Rodrigues', 'raulafonso88@gmail.com', '20200122_091237-01.jpeg', 'administrator', ''),
+(2, 'carla', 'xela', 'Carla', 'Caetano', 'carlacaetano3@gmail.com', 'carla.png', 'subscriber', NULL),
+(4, 'marcus', 'xela', 'Marcus', 'Aurelius', 'rome@mail.com', 'bi_graphics_rules-for-being-an-exceptional-leader-from-marcus-aurelius_top_03.png', 'subscriber', NULL);
 
 --
 -- Indexes for dumped tables
@@ -84,10 +132,22 @@ ALTER TABLE `categories`
   ADD PRIMARY KEY (`cat_id`);
 
 --
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`comment_id`);
+
+--
 -- Indexes for table `posts`
 --
 ALTER TABLE `posts`
   ADD PRIMARY KEY (`post_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -97,13 +157,25 @@ ALTER TABLE `posts`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `cat_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `cat_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `comment_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `post_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `post_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
