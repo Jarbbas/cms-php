1bc96a9900675d79d2dbd4d6ac8a1752af3f5922
Emanuel <raulafonso88@gmail.com>
2020-05-11 18:03:54 +0100

    added dropdown options for multiple optinos in post section

--- admin/includes/components/view_all_posts.php
+++ admin/includes/components/view_all_posts.php
@@ -1,12 +1,41 @@
+<?php
+
+  if (isset($_POST['checkBoxesArray'])) {
+
+        foreach ($_POST['checkBoxesArray'] as $checkBoxValue) {
+
+              bulkOptions($checkBoxValue);
+          }
+    }
+
+
+?>
+<form class="" action="" method="post">
+
+  <div id="bulkOptionsContainer" class="col-xs-4">
+    <select class="form-control" name="bulkOptions" id="">
+        <option value="">Select Options</option>
+        <option value="published">Publish</option>
+        <option value="draft">Draft</option>
+          <option value="delete">Delete</option>
+    </select>
+  </div>
+
+  <div class="col-xs-4">
+    <input type="submit" name="submit" class="btn btn-success" value="Apply">
+    <a href="posts.php?source=add" class="btn btn-primary">Add New</a>
+  </div>
+
 <table class="table table-bordered table-hover">
    <thead>
        <tr>
+           <th><input type="checkbox" id="selectAllBoxes"></th>
            <th>Id</th>
            <th>Author</th>
            <th>Title</th>
            <th>Category</th>
            <th>Status</th>
-           <th>Image</th>
+           <th>Ima/form>ge</th>
            <th>Tags</th>
            <th>Comments</th>
            <th>Date</th>
@@ -30,10 +59,16 @@ while ($row = mysqli_fetch_assoc($resultAllPosts)) {
         $post_comment_count = $row['post_comment_count'];
         $post_date = $row['post_date'];
 
-    echo "<tr>
-    <td>{$post_id}</td>
-    <td>{$post_author}</td>
-    <td>{$post_title}</td>";
+
+    ?>
+      <tr>
+      <td><input type="checkbox" class="checkBoxes" name="checkBoxesArray[]" value="<?php echo $post_id; ?>"></td>
+
+    <?php
+
+    echo "<td>{$post_id}</td>
+          <td>{$post_author}</td>
+          <td><a href='../post.php?post_id={$post_id}'>{$post_title}</td>";
 
     $query = "SELECT * FROM `categories` WHERE `cat_id` = '{$post_category_id}' ";
     $resultCatName= mysqli_query($connection, $query);
@@ -60,7 +95,6 @@ while ($row = mysqli_fetch_assoc($resultAllPosts)) {
     <td><a href='posts.php?delete={$post_id}'>Delete</a></td>
     </tr>";
 
-
 }
 
 if(isset($_GET['delete'])) {
@@ -72,3 +106,4 @@ if(isset($_GET['delete'])) {
 ?>
 </tbody>
 </table>
+</form>
