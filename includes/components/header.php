2e1cb237ace830b9a0e3fd3b49b3d73ad85cc737
Emanuel Rodrigues <raulafonso88@gmail.com>
2020-05-14 20:52:37 +0100

    added link to css

--- includes/components/header.php
+++ includes/components/header.php
@@ -30,17 +30,17 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="description" content="CMS aplication">
+    <meta name="author" content="Emanuel">
 
-    <title>Blog Home - Start Bootstrap Template</title>
+    <title>CMS PHP Project</title>
 
     <!-- Bootstrap Core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom CSS -->
     <link href="css/blog-home.css" rel="stylesheet">
-
+    <link href="css/style.css" rel="stylesheet">
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
