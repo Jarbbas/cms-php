d6fb31a6b2ec3fd3258d6f84750ffbd190fa2818
Emanuel <raulafonso88@gmail.com>
2020-05-15 15:29:35 +0100

    added extras fields in registration

--- registration.php
+++ registration.php
@@ -9,10 +9,17 @@ include_once($path_to_header);
 
   if (isset($_POST['submit'])) {
 
-    if (!empty($_POST['username']) && !empty($_POST['user_password']) && !empty($_POST['user_email'])) {
-         userRegistration();
+    if ($_POST['user_password'] === $_POST['password_validation']) {
+
+      if (!empty($_POST['username']) && !empty($_POST['user_password']) && !empty($_POST['user_email'])) {
+           userRegistration();
+      } else {
+          echo "<script>alert('Fields cannot be empty!')</script>";
+      }
+
     } else {
-        echo "<script>alert('Fields cannot be empty!')</script>";
+
+      echo "<script>alert('Your passwords dont match!')</script>";
     }
 
   }
@@ -35,15 +42,26 @@ include_once($path_to_navigation);
                             <label for="username" class="sr-only">username</label>
                             <input type="text" name="username" id="username" class="form-control" placeholder="Enter Desired Username">
                         </div>
+                        <div class="form-group">
+                            <label for="username" class="sr-only">user_fristname</label>
+                            <input type="text" name="user_fristname" id="user_fristname" class="form-control" placeholder="Enter your Frist Name">
+                        </div>
+                        <div class="form-group">
+                            <label for="username" class="sr-only">user_lastname</label>
+                            <input type="text" name="user_lastname" id="user_lastname" class="form-control" placeholder="Enter your Last Name">
+                        </div>
                          <div class="form-group">
                             <label for="email" class="sr-only">Email</label>
-                            <input type="email" name="user_email" id="email" class="form-control" placeholder="somebody@example.com">
+                            <input type="email" name="user_email" id="user_email" class="form-control" placeholder="somebody@example.com">
                         </div>
                          <div class="form-group">
                             <label for="password" class="sr-only">Password</label>
-                            <input type="password" name="user_password" id="key" class="form-control" placeholder="Password">
+                            <input type="password" name="user_password" id="user_password" class="form-control" placeholder="Desired Password">
                         </div>
-
+                        <div class="form-group">
+                           <label for="password" class="sr-only">confirm password</label>
+                           <input type="password" name="password_validation" id="password_validation" class="form-control" placeholder="Confirm Password">
+                       </div>
                         <input type="submit" name="submit" id="btn-login" class="btn btn-success btn-lg btn-block" value="Register">
                     </form>
 
